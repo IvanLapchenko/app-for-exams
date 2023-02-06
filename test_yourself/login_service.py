88948ee984cb16096a52f05f89c3ef80b5a173f3
IvanLapchenko <lapchenko.vanya@gmail.com>
2023-02-06 21:59:41 +0200

    Create class User

--- test_yourself/login_service.py
+++ test_yourself/login_service.py
@@ -1,4 +1,5 @@
 import sqlite3
+from random import randint
 
 connection = sqlite3.connect("app.db", check_same_thread=False)
 cursor = connection.cursor()
@@ -9,3 +10,10 @@ def check_if_user_exists_return_password(name):
     password = cursor.fetchall()[0][0] #this is here cause req returns list of tuples
     if len(password) == 1: password = None
     return password
+
+
+class User():
+    def __init__(self, name, password):
+        id = randint(10000, 99999)
+        self.name = name
+        self.password = password
